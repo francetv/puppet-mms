4b63b31b07b510b22dda1f1ccd5e0fc222c54997
btl <btl@claptrap>
2014-09-24 10:30:15 +0200

    mof lien mms

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,6 +1,6 @@
 class mms::params {
   $install_dir = '/opt/mms'
-  $download_url = 'https://mms.mongodb.com/settings/mms-monitoring-agent.tar.gz'
+  $download_url = 'https://mms.mongodb.com/download/agent/monitoring/mongodb-mms-monitoring-agent-2.5.0.116-1.linux_x86_64.tar.gz'
   $tmp_dir = '/tmp'
   $mms_server = 'https://mms.mongodb.com'
   $mms_user = 'mms'
