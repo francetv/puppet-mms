300043ced06185a8acf299f76bc5441958609496
btl <bertrand.thill@francetv.fr>
2016-02-17 14:07:04 +0100

    uni

--- manifests/init.pp
+++ manifests/init.pp
@@ -42,7 +42,7 @@
 #
 class mms (
   $api_key,
-  $install_dir  = '/usr/bin/', #$mms::params::install_dir,
+  $install_dir  = '/opt/mms', #$mms::params::install_dir,
   $tmp_dir      = $mms::params::tmp_dir,
   $mms_server   = $mms::params::mms_server,
   $mms_user     = $mms::params::mms_user
@@ -54,74 +54,64 @@ class mms (
     ensure => installed
   }
 
-#  file { $install_dir:
-#    ensure  => directory,
-#    mode    => '0755',
-#    recurse => true,
-#    owner   => $mms_user,
-#    group   => $mms_user,
-#    require => User[$mms_user]
-#  }
+  file { $install_dir:
+    ensure  => directory,
+    mode    => '0755',
+    recurse => true,
+    owner   => $mms_user,
+    group   => $mms_user,
+    require => User[$mms_user]
+  }
 
   user { $mms_user :
     ensure => present
   }
-   
 
-#  file { '/opt/mms/mongodb-mms-monitoring-agent':
-#    source  => "puppet:///modules/mms/opt/mms/mongodb-mms-monitoring-agent",
-#    mode    => '0754',
-#    owner   => $mms_user,
-#    group   => $mms_user,
-#    require => [File[$install_dir]]
-#  }
+  file { '/opt/mms/mongodb-mms-monitoring-agent':
+    source  => "puppet:///modules/mms//opt/mms/mongodb-mms-monitoring-agent",
+    mode    => '0754',
+    owner   => $mms_user,
+    group   => $mms_user,
+    require => [File[$install_dir]]
+  }
 
-  file { '/etc/mongodb-mms/monitoring-agent.config':
-    source  => "puppet:///modules/mms/opt/mms/monitoring-agent.config",
+  file { '/opt/mms/monitoring-agent.config':
+    source  => "puppet:///modules/mms//opt/mms/monitoring-agent.config",
     mode    => '0554',
     owner   => $mms_user,
     group   => $mms_user,
-    require => [Exec['package-init']]
+    require => [File[$install_dir]]
   }
 
-  file { '/etc/mongodb-mms/mongodb-mms.pl':
+  file { '/opt/mms/mongodb-mms.pl':
     source  => "puppet:///modules/mms/opt/mms/mongodb-mms.pl",
     mode    => '0754',
     owner   => $mms_user,
     group   => $mms_user,
-    notify  => Service['mongodb-mms'],
-    require => [Exec['package-init']]
+    require => [File[$install_dir]]
   }
 
 
-  exec  { 'package-install':
-   command => "cd /tmp; curl -OL https://cloud.mongodb.com/download/agent/monitoring/mongodb-mms-monitoring-agent_latest_amd64.deb; dpkg -i mongodb-mms-monitoring-agent_latest_amd64.deb; rm mongodb-mms-monitoring-agent_latest_amd64.deb",
-   path    => ['/bin', '/usr/bin'],
-  }
-
-  exec { 'package-init':
-  command => "/bin/bash -c \"export PERL_MM_USE_DEFAULT=1\" ; /bin/bash -c \"export PERL_EXTUTILS_AUTOINSTALL=--defaultdeps\"; perl -MCPAN -e \"install Daemon::Control\"",
+  exec { 'package-install':
+  command => "export PERL_MM_USE_DEFAULT=1 ; export PERL_EXTUTILS_AUTOINSTALL=\"--defaultdeps\"; perl -MCPAN -e \"install Daemon::Control\"",
   path    => ['/bin', '/usr/bin'],
-  require => [Exec['package-install']]
   } 
- 
+
   exec { 'set-license-key':
-    command => "sed -ie 's|@API_KEY@|${api_key}|' /etc/mongodb-mms/monitoring-agent.config",
+    command => "sed -ie 's|@API_KEY@|${api_key}|' ${install_dir}/monitoring-agent.config",
     path    => ['/bin', '/usr/bin'],
-    require => [File['/etc/mongodb-mms/monitoring-agent.config']]
+    require => [File['/opt/mms/monitoring-agent.config']]
   }
 
   exec { 'set-mms-server':
-    command => "sed -ie 's|@MMS_SERVER@|${mms_server}|' /etc/mongodb-mms/monitoring-agent.config",
+    command => "sed -ie 's|@MMS_SERVER@|${mms_server}|' ${install_dir}/monitoring-agent.config",
     path    => ['/bin', '/usr/bin'],
-    require => [File['/etc/mongodb-mms/monitoring-agent.config']]
+    require => [File['/opt/mms/monitoring-agent.config']]
   }
 
   file { '/etc/init.d/mongodb-mms':
     content => template('mms/etc/init.d/mongodb-mms.erb'),
-    mode    => 0755,
-    owner   => 'root',
-     group   => 'root',
+    mode    => '0744',
     require => [Exec['set-license-key'], Exec['set-mms-server']]
   }
 
