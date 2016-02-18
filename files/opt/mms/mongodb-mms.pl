37ab218d047d7143655510031b34b529f2df9d9d
btl <bertrand.thill@francetv.fr>
2016-02-18 08:06:46 +0100

    --

--- files/opt/mms/mongodb-mms.pl
+++ files/opt/mms/mongodb-mms.pl
@@ -3,7 +3,7 @@ use warnings;
 use strict;
 use Daemon::Control;
 my $process=2;
- 
+
 exit Daemon::Control->new(
     name        => "MMS agent",
     lsb_start   => '$syslog $remote_fs',
@@ -13,13 +13,13 @@ exit Daemon::Control->new(
     lsb_desc    => 'Controleur MMS etendu',
     user        => 'mms',
     group       => 'mms',
-    path        => '/opt/mms',
-    directory   => '/opt/mms', 
-    program     => '/opt/mms/mongodb-mms-monitoring-agent',
-    program_args => [ '> /dev/null 2>&1' ],
- 
+    path        => '/usr/bin',
+    directory   => '/usr/bin', 
+    program     => '/usr/bin/mongodb-mms-monitoring-agent',
+    program_args => [ '> /dev/null 2>&1' ], 
     pid_file    => '/tmp/mms_agent.pid',
     stderr_file => '/tmp/mms_agent.out',
     stdout_file => '/tmp/mms_agent.out', 
     fork        => $process,
 )->run;
+
